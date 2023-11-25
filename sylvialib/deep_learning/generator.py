ca86c7c00ce90711df07675ee7befe6ffce60ba1
SylviaWhittle <sylviwhittle@gmail.com>
2023-11-25 21:40:28 +0000

    Improve batch generator function with documentation, comments, optional file type

--- sylvialib/deep_learning/generator.py
+++ sylvialib/deep_learning/generator.py
@@ -1,14 +1,45 @@
 """A generator for deep learning models"""
 
+import random
+from pathlib import Path
+
 import cv2
 import numpy as np
-import random
-import re
 from PIL import Image
 
 
 # An image generator that loads images as they are needed
-def image_generator(MASK_DIR, ORIGINAL_IMAGE_DIR, image_indexes, batch_size=4):
+def image_generator(
+    original_image_dir: Path,
+    mask_dir: Path,
+    image_indexes: list,
+    batch_size: int = 4,
+    file_type: str = ".npy",
+):
+    """A generator that yields batches of images and ground truth masks.
+
+    Parameters
+    ----------
+    original_image_dir : Path
+        The directory containing the original images.
+    mask_dir : Path
+        The directory containing the ground truth masks.
+    image_indexes : list
+        A list of the indices of the images to be loaded.
+    batch_size : int, optional
+        The number of images to be loaded per batch. The default is 4.
+    file_type : str, optional
+        The file type of the images. The default is ".npy".
+
+    Yields
+    ------
+    batch_x : np.ndarray
+        A batch of images.
+    batch_y : np.ndarray
+        A batch of ground truth masks.
+
+    """
+
     while True:
         # Select files (paths/indices) for the batch
         batch_image_indexes = np.random.choice(a=image_indexes, size=batch_size)
@@ -17,10 +48,14 @@ def image_generator(MASK_DIR, ORIGINAL_IMAGE_DIR, image_indexes, batch_size=4):
 
         # Load the image and ground truth
         for index in batch_image_indexes:
-            # Find the index as the only number in the filename
-            # index = re.search(r"\d+", image_path)
-            # print(index)
-            image = cv2.imread(str(ORIGINAL_IMAGE_DIR / f"training_image_{index}.png"), 0)
+            # Load the training image
+            if file_type == ".npy":
+                image = np.load(original_image_dir / f"image_{index}.npy")
+            elif file_type == ".png":
+                image = cv2.imread(str(original_image_dir / f"image_{index}.png"), 0)
+            else:
+                raise ValueError("File type must be either .npy or .png")
+            # Rescale the image to 512x512
             image = Image.fromarray(image)
             image = image.resize((512, 512))
             image = np.array(image)
@@ -28,16 +63,22 @@ def image_generator(MASK_DIR, ORIGINAL_IMAGE_DIR, image_indexes, batch_size=4):
             image = image - np.min(image)
             image = image / np.max(image)
 
-            # ground_truth = np.load(MASK_DIR / f"mask_array_{index}.npy")
-            ground_truth = cv2.imread(str(MASK_DIR / f"mask_{index}.png"), 0)
-            ground_truth = Image.fromarray(ground_truth)
+            # Load the ground truth
+            if file_type == ".npy":
+                ground_truth = np.load(mask_dir / f"mask_{index}.npy")
+            elif file_type == ".png":
+                ground_truth = cv2.imread(str(mask_dir / f"mask_{index}.png"), 0)
+            else:
+                raise ValueError("File type must be either .npy or .png")
             ground_truth = np.array(ground_truth)
+            # Force the ground truth to be boolean
             ground_truth = ground_truth.astype(bool)
+            # Rescale the image to 512x512
             ground_truth = Image.fromarray(ground_truth.astype(np.uint8))
             ground_truth = ground_truth.resize((512, 512))
             ground_truth = np.array(ground_truth).astype(int)
 
-            # Augment the images
+            # Augment the image and ground truth
             # Flip the images 50% of the time
             if random.choice([0, 1]) == 1:
                 image = np.flip(image, axis=1)
@@ -47,9 +88,11 @@ def image_generator(MASK_DIR, ORIGINAL_IMAGE_DIR, image_indexes, batch_size=4):
             image = np.rot90(image, rotation)
             ground_truth = np.rot90(ground_truth, rotation)
 
+            # Add the image and ground truth to the batch
             batch_input.append(image)
             batch_output.append(ground_truth)
 
+        # Force the batch to be numpy arrays
         batch_x = np.array(batch_input).astype(np.float32)
         batch_y = np.array(batch_output).astype(np.float32)
 
