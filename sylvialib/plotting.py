c9487d2610000a43cfbab5af4c40bed44a68b4d3
SylviaWhittle <sylviwhittle@gmail.com>
2023-12-15 23:57:22 +0000

    Add image gallery function

--- sylvialib/plotting.py
+++ sylvialib/plotting.py
@@ -9,3 +9,16 @@ def imshow(image: np.ndarray, size=(8, 8)):
     plt.figure(figsize=size)
     plt.imshow(image)
     plt.show()
+
+
+def plot_gallery(images: list[np.ndarray], n_cols=4):
+    """Plot a list of images in a grid."""
+
+    n_images = len(images)
+    n_rows = np.ceil(n_images / n_cols)
+
+    _fig, axes = plt.subplots(n_rows, n_cols, figsize=(20 * n_rows, 20))
+    for index, image in enumerate(images):
+        axes[int(index // n_cols), index % n_cols].imshow(image)
+
+    plt.show()
