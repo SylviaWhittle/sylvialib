7413e635ccf13af248ac26a493dcf8a9d7c62738
SylviaWhittle <sylviwhittle@gmail.com>
2023-12-25 23:46:51 +0000

    Fix issues in file rename and add file_rename sequential for files with numbers in the name

--- sylvialib/deep_learning/file_management.py
+++ sylvialib/deep_learning/file_management.py
@@ -29,8 +29,59 @@ def rename_files(path: Path, file_ext: str, file_name_base: str):
         print("No files found")
         sys.exit()
     else:
-        for file in files:
+        # First pass, rename to temporary name to avoid overwriting
+        for i, file in enumerate(files):
+            temp_filename = path / f"temp_{i}{file_ext}"
+            print(f"Renaming {file.name} to {temp_filename}")
+            file.rename(temp_filename)
+
+        # Second pass, rename to desired name
+        temp_files = list(path.glob("*" + file_ext))
+        temp_files.sort()
+        for i, file in enumerate(temp_files):
             new_filename = path / f"{file_name_base}_{i}{file_ext}"
             print(f"Renaming {file.name} to {new_filename}")
             file.rename(new_filename)
-            i += 1
+
+
+def file_rename_numerical(path: Path, file_ext: str, file_name_base: str):
+    """Renames all files in a directory. Sorts the files in order of numbers that appear
+    in the existing file names.
+
+    Eg: [image_1.png, image_5.png, image_8.png] -> [image_0.png, image_1.png, image_2.png]
+    """
+
+    # Ensure that the path is a directory
+    if not path.is_dir():
+        print("Path is not a directory")
+        sys.exit()
+
+    # Ensure that the file extension starts with a period
+    if file_ext[0] != ".":
+        file_ext = "." + file_ext
+
+    i = 0
+
+    # Get all files in the directory
+    files = list(path.glob("*" + file_ext))
+    # Sort the files by the number that appears in the filename
+    files.sort(key=lambda f: int("".join(filter(str.isdigit, f.name))))
+
+    if len(files) == 0:
+        print("No files found")
+        sys.exit()
+    else:
+        # First pass, rename to temporary name to avoid overwriting
+        for i, file in enumerate(files):
+            temp_filename = path / f"temp_{i}{file_ext}"
+            print(f"Renaming {file.name} to {temp_filename}")
+            file.rename(temp_filename)
+
+        # Rename the files
+        temp_files = list(path.glob("*" + file_ext))
+        temp_files.sort(key=lambda f: int("".join(filter(str.isdigit, f.name))))
+
+        for i, file in enumerate(temp_files):
+            new_filename = path / f"{file_name_base}_{i}{file_ext}"
+            print(f"Renaming {file.name} to {new_filename}")
+            file.rename(new_filename)
