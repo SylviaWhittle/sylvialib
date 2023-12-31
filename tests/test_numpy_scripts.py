38708e55dc60cb169e2f2f4b9833176fc7a5e845
SylviaWhittle <sylviwhittle@gmail.com>
2023-12-31 22:00:25 +0000

    Add a test for find adjacent pixels

--- tests/test_numpy_scripts.py
+++ tests/test_numpy_scripts.py
@@ -2,7 +2,36 @@
 
 import numpy as np
 
-from sylvialib.numpy_scripts import create_2d_array_from_string
+from sylvialib.numpy_scripts import create_2d_array_from_string, find_touching_pixels
+
+
+def test_find_touching_pixels():
+    """Test the find_touching_pixels function"""
+
+    image = np.array(
+        [
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1],
+            [0, 1, 2, 2, 2, 0, 1],
+            [0, 1, 1, 1, 0, 0, 1],
+        ]
+    )
+
+    touching_pixels = find_touching_pixels(image)
+
+    assert np.array_equal(
+        touching_pixels,
+        np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 1, 1, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 1, 1, 0, 0, 0],
+            ]
+        ),
+    )
 
 
 def test_create_2d_array_from_string():
