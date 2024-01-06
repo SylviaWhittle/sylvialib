828497bdfedf1fcdc291b0806bbe0cb3750a0b54
SylviaWhittle <sylviwhittle@gmail.com>
2024-01-06 23:53:11 +0000

    Add curvature calculation

--- sylvialib/numpy_scripts.py
+++ sylvialib/numpy_scripts.py
@@ -3,6 +3,7 @@
 from typing import List, Tuple
 
 import numpy as np
+from scipy.interpolate import UnivariateSpline
 
 from scipy.ndimage import binary_dilation
 
@@ -110,3 +111,37 @@ def detect_overlap(mask_1: np.ndarray, mask_2: np.ndarray):
     """Detect if two masks overlap and return the overlapping image"""
 
     return np.logical_and(mask_1, mask_2).any()
+
+
+def calculate_curvature_from_points(x_points, y_points, error=0.1, k=4):
+    """Calculate the curvature for a set of points"""
+    # Check that the number of points is the same for both x and y
+    if x_points.shape[0] != y_points.shape[0]:
+        raise ValueError(
+            "x_points and y_points must have the same number of points."
+            f"x_points has {x_points.shape[0]} points and y_points has {y_points.shape[0]} points."
+        )
+
+    # Weight the values so less weight is given to points with higher error
+    # K is the order of the spline to use. Increasing this increases the smoothness of the spline. A
+    # value of 1 is linear interpolation, 2 is quadratic, 3 is cubic, etc. 4 is used to ensure the
+    # spline is smooth enough to differentiate to the second derivative.
+    # t is the independent variable that monotically increases with the data, similar to how
+    # we use a dummy x variable in plotting calculations.
+
+    # Disable pylint warning about snake case variable names for this function
+    # pylint: disable=invalid-name
+    t = np.arange(x_points.shape[0])
+    weight_values = 1 / np.sqrt(error * np.ones_like(x_points))
+    fx = UnivariateSpline(t, x_points, k=k, w=weight_values)
+    fy = UnivariateSpline(t, y_points, k=k, w=weight_values)
+
+    spline_x = fx(t)
+    spline_y = fy(t)
+
+    dx = fx.derivative(1)(t)
+    dx2 = fx.derivative(2)(t)
+    dy = fy.derivative(1)(t)
+    dy2 = fy.derivative(2)(t)
+    curvatures = (dx * dy2 - dy * dx2) / np.power(dx**2 + dy**2, 3 / 2)
+    return curvatures, spline_x, spline_y
